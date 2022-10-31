5d997f1e78be5a0ce084034017c0e282ae3fc5c6
Otaye <otayeanastasia@gmail.com>
2022-10-31 21:24:58 +0100

    Added images and icons

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,33 +1,98 @@
+import profileImg from "../assets/profile__img.svg";
+import slackIcon from "../assets/slack.svg";
+import githubIcon from "../assets/Icon.svg";
+import zuriIcon from "../assets/Zuri.Internship_Logo.svg";
+import ingressiveIcon from "../assets/I4G.svg";
+
 const Profile = () => {
-    return (
-        <div>
-            <img id="profile_img" alt="profile"/>
-            <h1>Annette Black</h1>
+  return (
+    <div>
+      <img src={profileImg} id="profile_img" alt="profile" />
+      <h1>Annette Black</h1>
 
-            <div className="links">
-                <a href="https://twitter.com/anaotaye" target="_blank" className="link" rel="noreferrer">Twitter Link</a><br/>
+      <div className="links">
+        <a
+          href="https://twitter.com/anaotaye"
+          target="_blank"
+          className="link"
+          rel="noreferrer"
+        >
+          Twitter Link
+        </a>
+        <br />
 
-                <a href="https://training.zuri.team/" target="_blank" id="btn__zuri" className="link" rel="noreferrer">Zuri Team</a><br/>
+        <a
+          href="https://training.zuri.team/"
+          target="_blank"
+          id="btn__zuri"
+          className="link"
+          rel="noreferrer"
+        >
+          Zuri Team
+        </a>
+        <br />
 
-                <a href="http://books.zuri.team/" className="link" target="_blank" rel="noreferrer">Zuri Books</a><br/>
+        <a
+          href="http://books.zuri.team/"
+          className="link"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Zuri Books
+        </a>
+        <br />
 
-                <a href="https://books.zuri.team/python-for-beginners?ref_id=anastasiaotaye" id="book__python" className="link" target="_blank" rel="noreferrer">Python Books</a><br/>
+        <a
+          href="https://books.zuri.team/python-for-beginners?ref_id=anastasiaotaye"
+          id="book__python"
+          className="link"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Python Books
+        </a>
+        <br />
 
-                <a href="https://background.zuri.team/" id="pitch" className="link" target="_blank" rel="noreferrer">Background Check for Coders</a><br/>
+        <a
+          href="https://background.zuri.team/"
+          id="pitch"
+          className="link"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Background Check for Coders
+        </a>
+        <br />
 
-                <a href="https://books.zuri.team/design-rules" id="book_design" className="link" target="_blank" rel="noreferrer">Design Books</a>
-            </div>
+        <a
+          href="https://books.zuri.team/design-rules"
+          id="book_design"
+          className="link"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Design Books
+        </a>
+      </div>
 
-            <div className="socials">
-                <a href="https://twitter.com/anaotaye"  id="twitter">Twitter</a>
-                <a href="https://twitter.com/anaotaye" id="slack">Slack</a>
-            </div>
+      <div className="socials">
+        <a href="https://twitter.com/anaotaye" id="twitter">
+          <img src={slackIcon} alt="slack link" />
+        </a>
+        <a href="https://twitter.com/anaotaye" id="slack">
+          <img src={githubIcon} alt="github link" />
+        </a>
+      </div>
 
-            <div className="footer">
+      <div className="footer">
+        <img src={zuriIcon} alt="Zuri internship logo" />
 
-            </div>
-        </div>
-    )
-}
+        <p>HNG Internship 9 Frontend Task</p>
+
+        <img src={ingressiveIcon} alt="Ingressive for good logo" />
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
